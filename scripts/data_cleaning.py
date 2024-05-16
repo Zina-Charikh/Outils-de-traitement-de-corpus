4d3a5a036666f8d3d88bad256908f93459ac5c65
Zina-Charikhs <zineb.charikh@gmail.com>
2024-05-16 15:05:35 +0200

    modif nettoyage

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -4,9 +4,10 @@ import shutil
 import re
 from pathlib import Path
 
-def clean_caption(text):
-    """ Supprimer les balises HTML du texte """
-    return re.sub(r'<[^>]*>', '', text)
+def clean_caption(text, alternative_text):
+    """ Supprimer les balises HTML du texte et remplacer par le texte alternatif si 'No description' """
+    text = re.sub(r'<[^>]*>', '', text)
+    return text if text.lower() != "no description" else alternative_text
 
 def extract_visual_name(url):
     """ Extraire le nom descriptif à partir de l'URL de l'image """
@@ -26,10 +27,14 @@ def main():
     # Charger le fichier CSV
     df = pd.read_csv(os.path.join(input_dir, 'animal_images_wikimedia.csv'))
 
+    # Pré-nettoyer les noms visuels
+    df['visual_cleaned'] = df['visual'].apply(extract_visual_name)
+
     # Nettoyer les données
-    df['caption'] = df['caption'].apply(clean_caption)
-    df['visual'] = df['visual'].apply(extract_visual_name)
-    
+    df['caption'] = df.apply(lambda row: clean_caption(row['caption'], row['visual_cleaned']), axis=1)
+    df['visual'] = df['visual_cleaned']
+    df.drop(columns=['visual_cleaned'], inplace=True)
+
     # Copier et renommer les images
     for idx, row in enumerate(df.itertuples(), 1):
         old_path = os.path.join(input_dir, 'images', f"{row.id}.jpg")
@@ -39,7 +44,6 @@ def main():
             shutil.copy(old_path, new_path)  # Copie au lieu de déplacer
 
         df.at[row.Index, 'id'] = f"{idx:02d}"
-        df.at[row.Index, 'visual'] = row.visual
 
     # Sauvegarder le nouveau fichier CSV
     df.to_csv(os.path.join(output_dir, 'animal_images_cleaned.csv'), index=False, encoding='utf-8')
